beb6a161f2ccca6db5cb3052646430714db1efeb
cyace84 <cyace84@gmail.com>
2023-06-25 05:30:55 +0200

    added template

---
+++ src/template/guides/sample-guide.md
@@ -0,0 +1,11 @@
+---
+outline: deep
+---
+
+# Sample Guide Page
+
+This is a sample guide page. You can use it as a template for your own guide pages.
+
+## Pack Data Example
+
+<PackDataSample />
---
+++ src/theme/broxusTheme.js
@@ -0,0 +1,24 @@
+import "./main.scss";
+
+import DefaultTheme from "vitepress/theme";
+
+import AppLayout from "./../components/AppLayout.vue";
+import Page from "./../components/Page.vue";
+import OutlineComponent from "./../components/shared/outline/Outline.vue";
+import OutlineItem from "./../components/shared/outline/OutlineItem.vue";
+import AccordionComponent from "./../components/shared/Accordion.vue";
+import DisconnectIcon from "./../components/shared/DisconnectIcon.vue";
+
+export default {
+  ...DefaultTheme,
+  Layout: AppLayout,
+  enhanceApp({ app }) {
+    DefaultTheme.enhanceApp({ app });
+    app.component("AppLayout", AppLayout);
+    app.component("Page", Page);
+    app.component("Outline", OutlineComponent);
+    app.component("OutlineItem", OutlineItem);
+    app.component("DisconnectIcon", DisconnectIcon);
+    app.component("Accordion", AccordionComponent);
+  },
+};
