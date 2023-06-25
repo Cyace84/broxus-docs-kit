37e6671bbfa7f053a578f67a62a165b743c9c77d
cyace84 <cyace84@gmail.com>
2023-06-25 05:09:04 +0200

    added themes components

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import DefaultTheme from "vitepress/theme";
+
+import WalletControl from "./WalletControl.vue";
+
+const { Layout: AppLayout } = DefaultTheme;
+</script>
+
+<template>
+  <AppLayout>
+    <template #nav-bar-content-after
+      ><Suspense><WalletControl /></Suspense
+    ></template>
+  </AppLayout>
+</template>
