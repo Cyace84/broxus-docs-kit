d2d169bb5b65591fca7db99aa47ebd326cc92db8
cyace84 <cyace84@gmail.com>
2023-06-28 08:07:45 +0200

    update eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import BDKLayout from './theme/components/BDKLayout.vue';
+import BDKPageVue from './theme/components/BDKPage.vue';
 import BDKWalletControl from './theme/components/BDKWalletControl.vue';
+import BDKAccordion from './theme/components/shared/BDKAccordion.vue';
+import BDKContractInfo from './theme/components/shared/BDKContractInfo.vue';
+import BDKEditableInput from './theme/components/shared/BDKEditableInput.vue';
+import BDKToast from './theme/components/shared/BDKToast.vue';
+import BDKOutline from './theme/components/shared/outline/BDKOutline.vue';
 
-export { BDKLayout, BDKWalletControl };
+export {
+  BDKLayout,
+  BDKWalletControl,
+  BDKPageVue,
+  BDKOutline,
+  BDKAccordion,
+  BDKContractInfo,
+  BDKEditableInput,
+  BDKToast,
+};
 
 export * from './providers/useProvider';
