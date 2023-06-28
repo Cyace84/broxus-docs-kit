f728cfad0a6238077e96404b5b8ba4fa56c33079
cyace84 <cyace84@gmail.com>
2023-06-28 10:04:27 +0200

    added client

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,99 +1,145 @@
 import fs from 'fs';
-import { copySync } from 'fs-extra';
 import path from 'path';
+import prompts from 'prompts';
+import tty from 'tty';
+import { copyTemplateFiles, createConfigFile } from './utils';
 
-import fileConfig from './source-map';
-
-interface ConfigParams {
-  docTitle: string;
-  projectName: string;
-  folderName: string;
-  HELP_URL: string;
-  FEEDBACK_URL: string;
-  GITHUB_URL: string;
-  componentsPath: string;
-  stylesPath: string;
-}
-export function createConfigFile(params: ConfigParams) {
-  const configUrl = path.join(params.folderName, '.vitepress/config.js');
-  let configFile = fs.readFileSync(configUrl, 'utf8');
-
-  for (const [key, value] of Object.entries(params)) {
-    configFile = configFile.replace(new RegExp(`{{${key}}}`, 'g'), value);
-  }
-
-  fs.writeFileSync(configUrl, configFile);
-}
-
-function copyFile(src: string, dest: string, mode: string) {
-  const file = path.basename(src);
-
-  if (file.endsWith(`.${mode}.js`) || file.endsWith(`.${mode}.scss`)) {
-    const newFileName = file.replace(`.${mode}`, ''); // Remove the mode from the filename.
-    fs.copyFileSync(src, path.join(dest, newFileName));
-  } else if (
-    !file.endsWith('.full.js') &&
-    !file.endsWith('.light.js') &&
-    !file.endsWith('.full.scss') &&
-    !file.endsWith('.light.scss')
-  ) {
-    fs.copyFileSync(src, dest);
-  }
-}
-
-function copyDirectory(src: string, dest: string, mode: string, copyRecursive: (src: string, dest: string) => void) {
-  fs.readdirSync(src).forEach(file => {
-    const srcFile = path.join(src, file);
-    const destFile = path.join(dest, file);
-
-    if (fs.statSync(srcFile).isDirectory()) {
-      if (!fs.existsSync(destFile)) {
-        fs.mkdirSync(destFile);
-      }
-      copyRecursive(srcFile, destFile);
-    } else {
-      copyFile(srcFile, destFile, mode);
+if (tty.isatty(0)) {
+  process.stdin.setRawMode(true);
+  process.stdin.on('keypress', function (chunk, key) {
+    if (key && key.name === 'c' && key.ctrl) {
+      // eslint-disable-next-line no-console
+      console.log('\n\nOperation cancelled by user.');
+      process.exit();
     }
   });
 }
 
-async function copyAdditionalFiles(folderName: string) {
-  for (const key in fileConfig) {
-    const src = fileConfig[key].src;
-    const dest = path.join(folderName, fileConfig[key].dest);
+async function main() {
+  const packageJson = JSON.parse(fs.readFileSync(path.join(process.cwd(), './package.json'), 'utf8'));
 
-    const srcStats = fs.lstatSync(src);
-    if (srcStats.isSymbolicLink()) {
-    } else if (srcStats.isDirectory()) {
-      copySync(src, dest);
-    } else {
-      const destDir = path.dirname(dest);
-      if (!fs.existsSync(destDir)) {
-        fs.mkdirSync(destDir, { recursive: true });
-      }
-      fs.copyFileSync(src, dest);
+  const response = await prompts([
+    {
+      type: 'text',
+      name: 'docTitle',
+      message: 'Enter the title of the documentation',
+      initial: packageJson.name + '-docs' || '',
+    },
+    {
+      type: 'text',
+      name: 'projectName',
+      message: 'Enter the name of the project to be documented',
+      initial: packageJson.name || '',
+    },
+    {
+      type: 'select',
+      name: 'mode',
+      message: 'Choose your mode',
+      choices: [
+        {
+          title: 'Light',
+          description: 'In Light mode, key components and styles will be imported from node modules.',
+          value: 'light',
+        },
+        {
+          title: 'Full',
+          description:
+            'In Full mode, everything will be installed in the user directory, but you must then keep track of updates yourself.',
+          value: 'full',
+        },
+      ],
+      initial: 0,
+    },
+    {
+      type: 'text',
+      name: 'folderName',
+      message: 'Enter the name of the folder where the template will be created',
+      initial: 'docs',
+    },
+    {
+      type: 'confirm',
+      name: 'askLinks',
+      message: 'Do you want to add links (GitHub, Feedback, etc.)?',
+      initial: false,
+    },
+  ]);
+
+  if (fs.existsSync(response.folderName)) {
+    const continueResponse = await prompts([
+      {
+        type: 'confirm',
+        name: 'continue',
+        message: `Directory ${response.folderName} already exists and is not empty. Continuing may overwrite existing files. Do you want to continue?`,
+        initial: false,
+      },
+    ]);
+    if (!continueResponse.continue) {
+      // eslint-disable-next-line no-console
+      console.log('Operation cancelled by user. Exiting...');
+      process.exit(0);
     }
   }
-}
 
-export async function copyTemplateFiles(folderName: string, mode: string) {
-  const templatePath = path.join(__dirname, '../../src/template');
+  let HELP_URL = 'https://t.me/everdev';
+  let FEEDBACK_URL = '';
+  let GITHUB_URL = packageJson.repository || '';
 
-  const copyRecursive = (src: string, dest: string) => {
-    if (fs.statSync(src).isDirectory()) {
-      copyDirectory(src, dest, mode, copyRecursive);
-    } else {
-      copyFile(src, dest, mode);
-    }
-  };
-
-  if (!fs.existsSync(folderName)) {
-    fs.mkdirSync(folderName);
+  if (response.askLinks) {
+    const linkResponse = await prompts([
+      {
+        type: 'text',
+        name: 'GITHUB_URL',
+        message: 'Enter the GitHub URL',
+        initial: packageJson.repository || ' ',
+      },
+      {
+        type: 'text',
+        name: 'FEEDBACK_URL',
+        message: 'Enter the feedback URL',
+      },
+      {
+        type: 'text',
+        name: 'HELP_URL',
+        message: 'Enter the help URL',
+        initial: 'https://t.me/everdev',
+      },
+    ]);
+    GITHUB_URL = linkResponse.GITHUB_URL;
+    FEEDBACK_URL = linkResponse.FEEDBACK_URL;
+    HELP_URL = linkResponse.HELP_URL;
   }
 
-  copyRecursive(templatePath, folderName);
+  const { docTitle, projectName, mode, folderName } = response;
 
-  if (mode === 'full') {
-    await copyAdditionalFiles(folderName);
-  }
+  console.log(
+    '\x1b[1m🚀 Creating\x1b[0m',
+    '\x1b[1m' + mode + '\x1b[0m',
+    '\x1b[1mmode documentation for project\x1b[0m',
+    '\x1b[1m"' + projectName + '"\x1b[0m',
+    '\x1b[1mwith title\x1b[0m',
+    '\x1b[1m"' + docTitle + '"\x1b[0m',
+    '\x1b[1min folder\x1b[0m',
+    '\x1b[1m"' + folderName + '"...\x1b[0m',
+    '📚'
+  );
+
+  await copyTemplateFiles(folderName, mode);
+
+  createConfigFile({
+    docTitle: docTitle,
+    projectName: projectName,
+    folderName: folderName,
+    HELP_URL: HELP_URL,
+    FEEDBACK_URL: FEEDBACK_URL,
+    GITHUB_URL: GITHUB_URL,
+    componentsPath: mode === 'light' ? 'broxus-docs-kit/dist/theme/components' : './theme/components',
+    stylesPath: mode === 'light' ? 'broxus-docs-kit/dist/theme/styles' : './theme/styles',
+  });
+
+  console.log('\n✅ Done!');
 }
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
